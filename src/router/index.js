1b9e567be05b67be4ef3cc0d733dbdae127dc16e
chenxudong <woshiyigemo129@gmail.com>
2021-03-12 11:48:32 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,26 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const router = new Router({
-  mode: 'history',
-  routes: [{
-    path: '/login',
-    name: 'login',
-    component: () => import( /* webpackChunkName: "login" */ '../views/login/login')
-  }, {
-    path: '/chat',
-    name: 'chat',
-    component: () => import( /* webpackChunkName: "chatroom" */ '../views/chat/chat')
-  }, {
-    path: '/',
-    redirect: '/login'
-  }]
+const routes = [{
+  path: '/login',
+  name: 'login',
+  component: () => import( /* webpackChunkName: "login" */ '../views/login/login')
+}, {
+  path: '/chat',
+  name: 'chat',
+  component: () => import( /* webpackChunkName: "chatroom" */ '../views/chat/chat')
+}]
 
+
+const notFound = [{
+  path: '*',
+  component: () => import( /* webpackChunkName: "404" */ '../views/404/404')
+}]
+
+
+routes.concat(notFound)
+
+export default new Router({
+  mode: 'history',
+  routes
 })
-export default router
---
+++ src/views/404/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="not-found">
+     <svg-icon icon-class="404" class='icon'/>
+     <p class='text'>很抱歉，您访问的页面不存在</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'notFound'
+}
+</script>
+
+<style lang='scss' scoped>
+.not-found {
+  .icon {
+    font-size: 260px;
+    margin: 10% auto 0;
+    display: block;
+  }
+  .text {
+    font-size: 14px;
+     text-align: center;
+  }
+}
+</style>
