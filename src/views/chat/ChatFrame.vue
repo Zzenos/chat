15f5f9ee62edfcd2e4b64b246ea1b5fba35cc90c
乐祥 <wanlexiang@zmeng123.com>
2021-03-25 21:08:44 +0800

    feat: 企微号列表UI

--- src/views/chat/ChatFrame.vue
+++ src/views/chat/ChatFrame.vue
@@ -3,7 +3,11 @@
     <a-row type="flex">
       <a-col flex="88px" class="side-bar">
         <svg-icon class-name="logo" icon-class="bizchat_logo"></svg-icon>
-        账号列表
+        <account-list />
+        <div class="bot-logo">
+          <svg-icon class-name="icon-user" icon-class="user_icon"></svg-icon>
+          <div>ZMENG</div>
+        </div>
       </a-col>
       <a-col flex="auto">
         <router-view />
@@ -13,8 +17,10 @@
 </template>
 
 <script>
+import AccountList from './components/AccountList'
 export default {
   name: 'chatFrame',
+  components: { AccountList },
   props: {
     // 企微saas账号
     saasId: {
@@ -29,12 +35,34 @@ export default {
 .side-bar {
   width: 88px;
   height: 100vh;
+  text-align: center;
   background: #272c33;
+  overflow: hidden;
   .logo {
-    margin-top: 20px;
-    margin-left: 20px;
-    width: 48px;
-    height: 48px;
+    position: sticky;
+    left: 0;
+    top: 0;
+    width: 88px;
+    height: 88px;
+    padding: 20px;
+    background: #272c33;
+  }
+  .bot-logo {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 88px;
+    height: 88px;
+    color: rgba(255, 255, 255, 0.45);
+    font-size: 12px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    background: #272c33;
+    .icon-user {
+      width: 20px;
+      height: 20px;
+      margin-top: 24px;
+      margin-bottom: 8px;
+    }
   }
 }
 </style>
--- src/views/chat/ContactList.vue
+++ src/views/chat/ContactList.vue
@@ -2,11 +2,17 @@
   <div>
     <a-row type="flex">
       <a-col flex="300px" class="contact-list">
-        这里是联系人列表
-        {{ userId }}
+        <div class="tab-container">
+          <div class="tab" :class="{ active: curTab === 1 }" @click="handleTab(1)">
+            <icon-font type="icontab_chat" class="icon" />
+            会话
+          </div>
+          <div class="tab" :class="{ active: curTab === 2 }" @click="handleTab(2)">
+            <icon-font type="icontab_addresslist_pre" class="icon" />
+            通讯录
+          </div>
+        </div>
         <a-button block type="primary">按钮</a-button>
-        <icon-font type="icontab_chat" class="icon-chat" />
-        <icon-font type="icontab_addresslist_pre" class="icon" />
       </a-col>
       <a-col flex="auto">
         <router-view />
@@ -24,6 +30,17 @@ export default {
       type: String,
       required: true
     }
+  },
+  data() {
+    return {
+      tabOptions: [],
+      curTab: 1
+    }
+  },
+  methods: {
+    handleTab(val) {
+      this.curTab = val
+    }
   }
 }
 </script>
@@ -34,13 +51,25 @@ export default {
   width: 300px;
   height: 100vh;
   background: #ffffff;
-  .icon {
-    color: #1d61ef;
-    font-size: 24px;
-  }
-  .icon-chat {
-    color: #888;
-    font-size: 24px;
+  .tab-container {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    height: 68px;
+    border-bottom: 1px solid #e4e5e7;
+    .tab {
+      width: 150px;
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.45);
+      cursor: pointer;
+      .icon {
+        font-size: 24px;
+        vertical-align: middle;
+      }
+      &.active {
+        color: rgba(0, 0, 0, 0.85);
+      }
+    }
   }
 }
 </style>
