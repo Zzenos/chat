7eba21205053fdfc372699e4aeaff0a130ff799f
chenxudong <woshiyigemo129@gmail.com>
2021-04-13 14:23:35 +0800

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import vue from 'vue'
 import vuex from 'vuex'
-// import vuexLocal from '@/plugins/vuex.persist'
+import vuexLocal from '@/plugins/vuex.persist'
 import messages from './messages'
 import chat from './chat'
 import contact from './contact'
@@ -11,8 +11,7 @@ vue.use(vuex)
 
 const store = new vuex.Store({
   state: {
-    token:
-      'eyJjb21wYW55Ijoi5piT5rGH5LyX55ufIiwiY29ycElkIjoid3dmYzNhZTU2MGVlMTU5MmQ4IiwiZGF0YUxpbWl0IjowLCJkZXB0SWQiOjAsImVudGVycHJpc2VJZCI6MSwiZXhwaXJlIjoxNjE4MjAwMjI2MjAxLCJpZCI6MTA1LCJyZWFsbmFtZSI6IuiBiuWkqeW3peWFtyIsInJvbGVJZCI6Mywic3RhdHVzIjowLCJ1c2VybmFtZSI6Imx0Z2oiLCJ1dWlkIjoiN2M3ZWM1MTRmZDEzNGE1NDg2NTY5OTNiMTFiYzhmNTcifQ=='
+    token: ''
   },
   mutations: {
     storeToken(state, token) {
@@ -25,7 +24,7 @@ const store = new vuex.Store({
     accounts,
     chat,
     contact
-  }
-  // plugins: [vuexLocal.plugin]
+  },
+  plugins: [vuexLocal.plugin]
 })
 export default store
