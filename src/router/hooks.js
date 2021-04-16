8cd5f1a97db999068c1afb2266e3a9b5d5ac088e
chenxudong <woshiyigemo129@gmail.com>
2021-04-16 15:36:17 +0800

    添加页面钩子

--- src/router/hooks.js
+++ src/router/hooks.js
@@ -5,11 +5,11 @@ export default {
   waitForStorageToBeReady: async (to, from, next) => {
     // if (to.path === '')
     // await store.restored
-    // console.log(6666666666, to, from, next)
+    console.log(6666666666, to, from, next)
     // 刷新需要重新回到
-    // if ((from.path === '/' || from.path === '/chatframe') && to.path !== '/chatframe' && to.path !== '/transit') {
-    //   next('/chatframe')
-    // }
+    if (from.path === '/' && to.path !== '/chatframe' && to.name !== '/Transit') {
+      next('/chatframe')
+    }
     next()
   }
 }
