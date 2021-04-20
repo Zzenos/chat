62deec605f33290a0b20831677c4039a4c8d2c30
chenxudong <woshiyigemo129@gmail.com>
2021-04-20 14:43:11 +0800

    fix

--- src/router/hooks.js
+++ src/router/hooks.js
@@ -5,7 +5,6 @@ export default {
   waitForStorageToBeReady: async (to, from, next) => {
     // if (to.path === '')
     // await store.restored
-    console.log(6666666666, to, from, next)
     // 刷新需要重新回到
     if (from.path === '/' && to.path !== '/chatframe' && to.name !== 'Transit') {
       next('/chatframe')
