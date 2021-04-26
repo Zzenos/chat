2790e93686591404cf25bc7b17752298a35cfb86
乐祥 <wanlexiang@zmeng123.com>
2021-04-26 23:52:33 +0800

    feat: 通讯录状态更新

--- src/store/contact.js
+++ src/store/contact.js
@@ -5,12 +5,12 @@ export default {
   state: {},
   mutations: {
     [types.ADD_CONTACT](state, contactsData) {
-      if (state[contactsData[0].tjId]) {
-        // 暂时缓存但不更新
-        // state[contactsData[0].tjId] = contactsData[0]
-      } else {
-        Vue.set(state, `${contactsData[0].tjId}`, contactsData[0])
-      }
+      // if (state[contactsData[0].tjId]) {
+      //   state[contactsData[0].tjId] = contactsData[0]
+      // } else {
+      //   Vue.set(state, `${contactsData[0].tjId}`, contactsData[0])
+      // }
+      Vue.set(state, `${contactsData[0].tjId}`, contactsData[0])
     }
   },
   actions: {},
@@ -18,6 +18,17 @@ export default {
     contactByTjId: state => {
       return tjId => {
         return state[tjId]
+      }
+    },
+    /**
+     * 获取x通讯录下x账号信息
+     * @param tjId 操作的账号对应的tjId
+     * @param wechatId 聊天的客户的tjId/wechatId
+     * @param tag customerList / groupList / memberList
+     */
+    contactInfoByWechatId: state => {
+      return (tjId, wechatId, tag = 'customerList') => {
+        return state[tjId] && state[tjId][tag] ? state[tjId][tag].find(item => item.wechatId === wechatId) : null
       }
     }
   }
