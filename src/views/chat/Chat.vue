f296adc9c4405c1418df7bd743689356a01f86b9
renyunfei <renyunfei@zmeng123.com>
2021-05-26 14:08:36 +0800

    语音转文字删除

--- src/views/chat/Chat.vue
+++ src/views/chat/Chat.vue
@@ -391,6 +391,14 @@ export default {
             this.translateText(index, item)
           }
         })
+        menus.push({
+          label: '删除',
+          icon: 'delete',
+          customClass: 'cus-contextmenu-item',
+          onClick: () => {
+            this.closeTranslateText(index, item)
+          }
+        })
       }
 
       menus.push({
