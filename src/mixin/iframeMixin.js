4934fcbdfef12854caf418929c83a9980e19ae01
乐祥 <wanlexiang@zmeng123.com>
2021-05-27 17:18:42 +0800

    优化

--- src/mixin/iframeMixin.js
+++ src/mixin/iframeMixin.js
@@ -22,11 +22,11 @@ export default {
       }
     }
   },
-  watch: {
-    activeKey(n, o) {
-      if (n === o || n == 'groupInfo') return
-    }
-  },
+  // watch: {
+  //   activeKey(n, o) {
+  //     if (n === o || n == 'groupInfo') return
+  //   }
+  // },
   mounted() {
     window.addEventListener('message', this.handleMessage, false)
   },
@@ -34,13 +34,19 @@ export default {
     window.removeEventListener('message', this.handleMessage, false)
   },
   methods: {
-    initIframe(refName) {
-      console.log('iframe ref:', refName, this.$refs[`${refName}`])
+    /**
+     * @description 初始化iframe实例
+     */
+    initIframe() {
+      if (!this.activeKey) return null
+      const refName = this.activeKey + 'Frame'
+      console.log('current iframe ref:', refName, this.$refs[`${refName}`])
       if (!this.$refs[`${refName}`]) {
-        this.sidebarConfig[this.activeKey].iframeWin = null
-        return
+        return null
+      } else {
+        this.sidebarConfig[this.activeKey].iframeWin = this.$refs[refName].contentWindow
+        return this.$refs[refName].contentWindow
       }
-      this.sidebarConfig[this.activeKey].iframeWin = this.$refs[refName].contentWindow
     },
     /**
      * @param {String} type
@@ -48,11 +54,10 @@ export default {
      * @description 传递数据给iframe
      */
     sendMessage(type, data) {
-      if (!this.activeKey) return
-      this.initIframe(`${this.activeKey}Frame`)
-      if (!this.sidebarConfig[this.activeKey].iframeWin) return
+      const iframeWin = this.initIframe()
+      if (!iframeWin) return
       setTimeout(() => {
-        this.sidebarConfig[this.activeKey].iframeWin.postMessage({ type, data }, '*')
+        iframeWin.postMessage({ type, data }, '*')
         console.warn('BizChat sendMessage', { type, data })
       }, 300)
     },
@@ -63,9 +68,9 @@ export default {
       console.warn('evt', evt.data)
       if (evt.origin.indexOf('bizchat') === -1 || !evt.data) return
       const { cmd, data } = JSON.parse(evt.data)
-      // 传递数据
       switch (cmd) {
         case 'ready':
+          // iframe 加载完成 传递用户数据
           this.sendMessage('InitUserInfo', this.userInfo)
           break
         case 'sendChatMessage':
